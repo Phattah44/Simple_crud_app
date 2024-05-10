5359a5fba9a8a52d76eaebed8472c65830d97506
Abdulfattah Sheu <elsheikh.saf@gmail.com>
2024-05-10 13:14:21 +0100

    get and post request done

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 
-app.listen(3001, () => {
-  console.log('server is running on port 3001');
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node API');
+  res.send('Hello from Node API Server');
 });
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://elsheikhsaf:7wZCZ74jFiLPjOxy@backenddb.bgodyzs.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB'
+  )
+  .then(() => {
+    console.log('Connected to the database!');
+    app.listen(3001, () => {
+      console.log('server is running on port 3001');
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
